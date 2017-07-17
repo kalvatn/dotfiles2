d49da43dfffe4290479cafcedd0a91ab84b04122
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2017-07-17 19:58:19 +0200

    remove old scripts

--- old-scripts/bootstrap.sh
+++ old-scripts/bootstrap.sh
@@ -8,20 +8,6 @@ function is_command() {
 function not_command() {
   ! is_command "$1"
 }
-function which_package() {
-  dpkg -S "$(which "$1")" | cut -d ':' -f1
-}
-
-IN_DOCKER=false
-if [ -f /.dockerenv ]; then
-  echo "RUNNING IN DOCKER CONTAINER"
-  if [ -z "$USER" ]; then
-    IN_DOCKER=true
-    export USER="docker"
-  fi
-fi
-
-echo "USER : $USER"
 
 if not_command apt-add-repository; then
   sudo apt-get install -y software-properties-common python-software-properties curl apt-transport-https
@@ -39,41 +25,6 @@ if not_command ansible; then
   sudo apt-get install ansible -y
 fi
 
-if ! $IN_DOCKER; then
-  if is_command docker; then
-    DOCKER_PACKAGE_INSTALLED="$(which_package "docker")"
-    if [ "$DOCKER_PACKAGE_INSTALLED" != "docker-ce" ]; then
-      echo "removing old docker versions"
-      sudo apt-get remove docker docker-engine docker.io -y
-    fi
-  fi
-
-  if not_command docker; then
-    echo "installing docker-ce"
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
-    sudo apt-get update -qq
-    sudo sudo apt-get install docker-ce -y
-
-
-    if ! id -nG $USER | grep -qw docker; then
-      echo "adding user $USER to group docker"
-      sudo usermod -a -G docker $USER
-    fi
-  fi
-
-  if ! id -nG $USER | grep -qw docker; then
-    read -p "relog required, logout now? (yes/no) "
-    if [ "$REPLY" == "yes" ]; then
-      echo "logging out.."
-      sleep 3
-      pkill -KILL -u $USER
-    else
-      echo "logout/login to apply user group changes"
-    fi
-  fi
-
-fi
 
 echo "done"
 exit 0
